6e3640a1c9ba67301250c1dfd0874da8d673092d
Vytautas Vosylius <vytautas.vosylius@mycit.ie>
2020-10-24 16:51:32 +0100

    Initial version of the Fin-Ai Android app. Features: Login & Registration via email, logging out, storing user information into firebase.

---
+++ app/src/main/java/com/example/fin_ai/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.fin_ai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // Allowing the user to logout.
+    public void logout(View view){
+        FirebaseAuth.getInstance().signOut(); // Logging the user out.
+        startActivity(new Intent(getApplicationContext(),Login.class)); // Send the user back to the login screen.
+        finish(); // Destroy the activity so it cannot be accessed after logging out.
+    }
+}
