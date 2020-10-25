10ebfad2e1c83e69b42508d36ddcc66c5b8c4ee7
Vytautas Vosylius <vytautas.vosylius@mycit.ie>
2020-10-25 21:49:11 +0000

    Added support for logging in with Google account.

---
+++ app/src/main/java/com/example/fin_ai/GoogleInfo.java
@@ -0,0 +1,116 @@
+package com.example.fin_ai;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.w3c.dom.Text;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GoogleInfo extends AppCompatActivity {
+
+    private static final String TAG = "TAG";
+    EditText mAddress;
+    Button mloginButton;
+    String userID;
+
+    FirebaseFirestore fStore;
+    FirebaseAuth fAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_info);
+
+        mAddress = findViewById(R.id.address);
+
+        mloginButton = findViewById(R.id.loginButton);
+        final GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
+
+        fStore = FirebaseFirestore.getInstance(); // Getting the current instance of the firestore database from firebase.
+        fAuth = FirebaseAuth.getInstance(); // Getting the current instance of the authentication database from firebase.
+
+//        DocumentReference mDocumentReference = fStore.collection("users").document(fAuth.getCurrentUser().getUid());
+//
+//        mDocumentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+//                                                           @Override
+//                                                           public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+//                                                               if (task.isSuccessful()) {
+//                                                                   DocumentSnapshot documentSnapshot = task.getResult();
+//                                                                   if (documentSnapshot != null && documentSnapshot.exists()) {
+//                                                                       Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+//                                                                       startActivity(intent);
+//                                                                   }
+//                                                               }
+//                                                           }
+//                                                       });
+
+        mloginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String firstName = signInAccount.getGivenName();
+                String surName = signInAccount.getFamilyName();
+                String address = mAddress.getText().toString();
+                String email = signInAccount.getEmail();
+
+                // Handling potential errors
+
+                if (TextUtils.isEmpty(address)){
+                    mAddress.setError("Address field cannot be empty.");
+                }
+                else if (!TextUtils.isEmpty(address)){
+
+                    // Storing user data into firestore and sending them to the Fin-Ai dashboard.
+                    Toast.makeText(GoogleInfo.this, "Account created!", Toast.LENGTH_LONG).show();
+                    userID = fAuth.getCurrentUser().getUid();; // Getting current user's userID to use it as a document ID in firestore.
+                    // Storing user date in firestore (database).
+                    DocumentReference mDocumentReference = fStore.collection("users").document(userID); // creates a collection "users" if it is not already present in firestore.
+                    // Creating a hashmap with key: String, value: Object.
+                    Map<String,Object> user = new HashMap<>();
+                    // Placing user information as a value into relevant key. (k,v)
+                    user.put("firstName", firstName);
+                    user.put("surName", surName);
+                    user.put("address", address);
+                    user.put("email", email);
+
+                    // Inserting data into firestore collection users; document UserID;
+                    mDocumentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Log.d(TAG, "User profile is created for: " + userID);
+                        }
+                    });
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+};
--- app/src/main/java/com/example/fin_ai/Login.java
+++ app/src/main/java/com/example/fin_ai/Login.java
@@ -6,35 +6,75 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Login extends AppCompatActivity {
 
+    private static final int RC_SIGN_IN = 1;
+    private static final String TAG = "TAG";
     Button mLoginButton, mRegisterButton, mReviewButton;
+    ImageButton mLoginGoogleButton;
     EditText mEmail, mPassword;
     FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    private GoogleSignInClient mGoogleSignInClient;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Check if a user is already authenticated, if this is the case don't bother showing the login screen.
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        // Buttons
         mLoginButton = findViewById(R.id.loginButton);
         mRegisterButton = findViewById(R.id.registerButton);
         mReviewButton = findViewById(R.id.reviewButton);
 
+        // Image buttons
+        mLoginGoogleButton = findViewById(R.id.googleLoginButton);
+
+        // EditText
         mEmail = findViewById(R.id.email);
         mPassword = findViewById(R.id.password);
 
         fAuth = FirebaseAuth.getInstance();  // Getting the current instance of the database from firebase.
+        mAuth = FirebaseAuth.getInstance(); // Getting the current instance of the database from firebase for use in the Google Sign in.
+        fStore = FirebaseFirestore.getInstance(); // Getting the current instance of the firestore database from firebase.
+
+        // Handling user login via email + password
 
         mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,6 +112,17 @@ public class Login extends AppCompatActivity {
                 }
         });
 
+        // Handling user login via Google
+
+        // Configure Google Sign In (This is used by GoogleSignInClient)
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
         // Sending the user to registration activity if and when Register button is clicked.
         mRegisterButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,6 +130,79 @@ public class Login extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), Register.class));
             }
         });
+
+
+        // Making the google imagebutton run the signIn() function when clicked which starts the signIn intent from mGoogleSignInClient
+        mLoginGoogleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+    }
+
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                // Google Sign In failed, display notification (toast) to user.
+                Toast.makeText(this, "Failed to sign in", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            DocumentReference mDocumentReference = fStore.collection("users").document(mAuth.getCurrentUser().getUid());
+
+                            mDocumentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                @Override
+                                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                    if (task.isSuccessful()) {
+                                        DocumentSnapshot documentSnapshot = task.getResult();
+                                        if (documentSnapshot != null && documentSnapshot.exists()) {
+                                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                            startActivity(intent);
+                                        }
+                                        else {
+                                            Intent intent = new Intent(getApplicationContext(), GoogleInfo.class);
+                                            startActivity(intent);
+                                        }
+                                    }
+                                }
+                            });
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(Login.this, "Authentication failed.", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 
     @Override
